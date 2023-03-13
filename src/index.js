1c04c78571e34aac3113729c578ea16d675e7951
JAFARY (DEVELE) <jaffarymdegela@gmail.com>
2023-03-13 14:39:37 +0300

    React Variable

--- src/index.js
+++ src/index.js
@@ -14,9 +14,21 @@ const root = ReactDOM.createRoot(el);
 // 4) Create a components
 
 function App() {
-  return <h1>Hi there</h1>;
+  // Passing variabe in Jsx
+  //react fail to render boolen value
+  const value = true  ;
+  // react object
+
+  
+  const arrayValue=[1,2,3]
+  let message = "Bye there !";
+  if (Math.random() > 0.5) {
+    message = "Hello there";
+  }
+
+  return <h1>{value}{arrayValue}</h1>;
+  
 }
 
 // 5) Show components on screen
-
 root.render(<App />);
